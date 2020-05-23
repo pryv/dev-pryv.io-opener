bbf6ed1b06bd3f358355803ccb493b1127f877ae
Perki <pm@perki.com>
2020-05-23 12:50:30 +0200

    most of api-server test passing and all of business

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-- update `src-dest/.api-version`
+# Pryv Service-Core Opener
+
+Script that extracts sources for the Open-Core version of Pryv.io
+
+# Usage
+
+1. checkout `service-core` and `app-node-opener` in the same directory
+2. eventually update `src-dest/.api-version` and set the version of the future open-source release
+3. run `npm run build`
+
+The **"Open-sourced version"** will be located in `./dest`
+
--- src/index.js
+++ src/index.js
@@ -10,14 +10,10 @@ const sed = require('./lib/sed')(destDir);
 
 const tasks = [{
     target: './components', 
-    excludes: ['hfs-server', 'pryvuser-cli', 'tprpc', 'webhooks',
+    excludes: ['hfs-server', 'pryvuser-cli', 'tprpc', 'webhooks', 'metadata',
       'business/src/series', 'business/src/series.js', 'repository.test.js'],
     patterns: ['-node_modules/','-*influx*', '-*series*','-webhook*']
   }, 
-  { // remove series and hooks from business
-    target: './components/business/src/index.js',
-    sed: ['series', 'hook']
-  },
   {
     target: './scripts',
     excludes: ['compile-proxy-config.js', 'components-checkdeps.js', 'components-version.js'],
@@ -28,7 +24,7 @@ const tasks = [{
   },
   {
     target: './package.json',
-    sed: ['hfs', 'metdata', 'webhooks', 'gnat', 'influx', 'jsdoc', 'test-root', 'cover', 'flow-coverage', 'tag-tests', 'test-results', 'tprpc', 'jaeger', 'pryvuser-cli']
+    sed: ['hfs', 'metdata', 'webhooks', 'gnat', 'influx', 'jsdoc', 'test-root', 'cover', 'flow-coverage', 'tag-tests', 'test-results', 'tprpc', 'jaeger', 'pryvuser-cli', 'metadata']
   },
   {
     target: './.flowconfig',
