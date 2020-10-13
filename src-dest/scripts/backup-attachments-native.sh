e0c9106e6c89af8a470400bf3b42b9765a777524
Ieva <ieva@pryv.com>
2020-10-13 13:40:59 +0200

    fix last slash error in backup script

--- src-dest/scripts/backup-attachments-native.sh
+++ src-dest/scripts/backup-attachments-native.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-# working dir fix
+# working dir
+PARENT_DIR=$(pwd)
+
+# build backup directory
+BACKUP_DIR=$(echo $1 | sed 's:/*$::')
+BACKUP_DIR="$BACKUP_DIR/"
+
 SCRIPT_FOLDER=$(cd $(dirname "$0"); pwd)
 cd $SCRIPT_FOLDER/..
 
-export VAR_PRYV_FOLDER=$SCRIPT_FOLDER/../var-pryv
-rsync --recursive --times --human-readable --verbose --perms ${VAR_PRYV_FOLDER}/attachment-files/ $1
+export VAR_PRYV_FOLDER=$PARENT_DIR/var-pryv
+
+rsync --recursive --times --human-readable --verbose --perms $BACKUP_DIR ${VAR_PRYV_FOLDER}/attachment-files/
 
