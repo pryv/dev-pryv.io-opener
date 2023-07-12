aefb615351dce473ddff8a54a22e3bc67ad66a53
Simon Goumaz <simon@attentif.ch>
2023-07-12 12:02:44 +0200

    Remove unneeded `lodash` and update dependencies

--- build/lib/json.js
+++ build/lib/json.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const fs = require('fs');
-const _ = require('lodash');
 
 module.exports = function (destDir) {
   return function (task) {
@@ -8,7 +7,7 @@ module.exports = function (destDir) {
     const jsonStringContent = fs.readFileSync(fileDest, { encoding: 'utf8' });
     let dest = JSON.parse(jsonStringContent);
     if (task.json.merge) {
-      dest = _.merge(dest, task.json.merge);
+      dest = Object.assign(dest, task.json.merge);
     }
     fs.writeFileSync(fileDest, JSON.stringify(dest, null, 2));
   };
